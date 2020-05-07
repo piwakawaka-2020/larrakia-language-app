a9b67655082b7ab80560641cfa880b3b24da3b55
Zachary <zachildred@gmail.com>
2020-05-07 14:45:27 +1200

    Add api calls to frontend

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,3 +1,5 @@
+import { getWords, getWordById, getWordsByCategory } from '../api'
+
 export const SAVE_WORDS = 'SAVE_WORDS'
 export const RECEIVE_WORDS = 'RECEIVE_WORDS'
 
@@ -9,7 +11,7 @@ const saveWords = words => {
   }
 }
 
-//This action replaces the list of words in state with a new list
+// This action replaces the list of words in state with a new list
 const receiveWords = words => {
   return {
     type: RECEIVE_WORDS,
@@ -17,29 +19,32 @@ const receiveWords = words => {
   }
 }
 
+// Gets all words from the database and replaces all words in state
 export const fetchWords = () => {
-  // return dispatch => {
-  //   APICALL()
-  //     .then(words => {
-  //       dispatch(receiveWords(words))
-  //     })
-  // }
+  return dispatch => {
+    getWords()
+      .then(words => {
+        dispatch(receiveWords(words))
+      })
+  }
 }
 
+// Gets a single word from the database and adds it to the list of words in state
 export const fetchWordById = () => {
-  // return dispatch => {
-  //   APICALL()
-  //     .then(words => {
-  //       dispatch(saveWords(words))
-  //     })
-  // }
+  return dispatch => {
+    getWordById()
+      .then(words => {
+        dispatch(saveWords(words))
+      })
+  }
 }
 
+// Gets a list of words from the database and adds them to the list of words in state
 export const fetchWordsByCategory = () => {
-  // return dispatch => {
-  //   APICALL()
-  //     .then(words => {
-  //       dispatch(saveWords(words))
-  //     })
-  // }
+  return dispatch => {
+    getWordsByCategory()
+      .then(words => {
+        dispatch(saveWords(words))
+      })
+  }
 }
--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
-
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
-const App = () => {
-  return (
-    <div className="app">
-      Hello World
-    </div>
-  )
+import { fetchWords } from '../actions'
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(fetchWords())
+  }
+
+  render() {
+    return (
+      <div className="app">
+        Hello World
+      </div>
+    )
+  }
 }
 
 export default connect()(App)
