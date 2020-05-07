761232632ab830c58a4c7976374de8a12b4f2c95
Boyd_Thurston <boydthurston@gmail.com>
2020-05-07 12:31:09 +1200

    words table migration

---
+++ server/db/db.js
@@ -0,0 +1,8 @@
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const connection = require('knex')(config)
+
+
+export function getWords (db = connection) {
+  return db('words').select()
+}
