15a98868fd2eacfd82774ba0b33baf981ca7a8df
Boyd_Thurston <boydthurston@gmail.com>
2020-05-07 13:03:27 +1200

    words table seed data

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,13 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: 'server/db/migrations'
+    },
+    seeds: {
+      directory: 'server/db/seeds'
+    }
   },
 
   test: {
