31c0597b11c4f415c56f249e1d4d4efa8b66675c
Boyd_Thurston <boydthurston@gmail.com>
2020-05-08 12:08:38 +1200

    MaMuk fix

--- server/db/seeds/words.js
+++ server/db/seeds/words.js
@@ -30,9 +30,9 @@ exports.seed = function(knex) {
         {gulumirrgin_word: 'Dadbinggwa', english_word: 'Sugarbag(honey)', category: 'Plants', image_url: '/images/Dadbinggwa.jpg' },
         {gulumirrgin_word: 'Dambilinggwa', english_word: 'Waterlilly', category: 'Plants', image_url: '/images/Dambilinggwa.png' },
         {gulumirrgin_word: 'Mayilema', english_word: 'Speargrass', category: 'Plants', image_url: '/images/Mayilema.jpg' },
-        {gulumirrgin_word: 'Nyirruck', english_word: 'Dad', category: 'Family', image_url: '/images/Nyirruck.jpg' },
+        {gulumirrgin_word: 'Nyirruck', english_word: 'Dad', category: 'Family', },
         {gulumirrgin_word: 'Batji', english_word: 'Good', category: 'Phrases', image_url: '/images/BatjiKoala.jpg' },
-        {gulumirrgin_word: 'Ma Muk', english_word: 'See you later', category: 'Phrases', },
+        {gulumirrgin_word: 'Ma Muk', english_word: 'See you later', category: 'Phrases', image_url: '/images/MaMuk.jpg' },
       ])
     })
 }
