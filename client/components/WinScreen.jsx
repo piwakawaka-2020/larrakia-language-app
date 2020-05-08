4603e15092c5f4d40eb26d3ec893fb8694cae41b
Richard Kwon <rkminjae@gmail.com>
2020-05-08 16:40:39 +1200

    Add confetti animation to WinScreen component

--- client/components/WinScreen.jsx
+++ client/components/WinScreen.jsx
@@ -1,22 +1,26 @@
+//Require import
 import React, { Component } from 'react'
+import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
 
-class WinScreen extends Component {
-    render() {
-        return (
-            <div className="container container-full center-align">
-                <div className="row">
-                    <div className="mx-auto">
-                        <h1>Well Done!</h1>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="mx-auto">
-                        <button>Press here to go back to Larrakia</button>
-                    </div>
+
+const WinScreen = () => {
+    const { width, height } = useWindowSize()
+    return (
+        <div className="container container-full center-align">
+            <Confetti width={width} height={height}/>
+            <div className="row">
+                <div className="mx-auto">
+                    <h1>Well Done!</h1>
                 </div>
             </div>
-        )
-    }
+            <div className="row">
+                <div className="mx-auto">
+                    <button>Press here to go back to Larrakia</button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default WinScreen
