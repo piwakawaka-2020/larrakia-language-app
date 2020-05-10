632e5d98d4cabcec687003214569f3a6ca8b680e
Richard Kwon <rkminjae@gmail.com>
2020-05-11 10:39:01 +1200

    Change class attribute to className

--- client/components/SuccessTick.jsx
+++ client/components/SuccessTick.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function SuccessTick() {
     return (
-        <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-            <circle class="checkmark-circle" cx="26" cy="26" r="25" fill="none"/>
-            <path class="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+        <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+            <circle className="checkmark-circle" cx="26" cy="26" r="25" fill="none"/>
+            <path className="checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
         </svg>
     )
 }
