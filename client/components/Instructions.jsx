839244d830c0771ca00d7a5e5dd5e95c7f7883d0
Richard Kwon <rkminjae@gmail.com>
2020-05-11 14:55:57 +1200

    Add comment to show what props children does

--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -5,6 +5,7 @@ const Instructions = ({ children }) => {
         <div>
             <h3>How to Play:</h3>
             <ol>
+                {/* Wrap <li> tags with <Instructions> tags */}
                 {children}
             </ol>
         </div>
