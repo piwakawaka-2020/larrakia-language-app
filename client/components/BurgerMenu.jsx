b40e6a34434fea65c511cfc68fcbc4144f5ec206
Laniyuk <laniyuk.info@gmail.com>
2020-05-11 15:49:14 +1200

    add working menu burger

--- client/components/BurgerMenu.jsx
+++ client/components/BurgerMenu.jsx
@@ -1,63 +1,37 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
 
 
 class BurgerMenu extends React.Component {
 
-    state = {
-      isShowing: false
-    }
+  state = {
+    isShowing: false
+  }
 
-    clickBurgerHandler = (event) => {
-      console.log("clicked")
-      this.setState({
-        isShowing: !this.state.isShowing
-      })
-    }
+  clickBurgerHandler = (event) => {
+    this.setState({
+      isShowing: !this.state.isShowing
+    })
+  }
 
-    render () {
-      console.log(this.state.isShowing)
-
-        // const hamburger = document.querySelector('.hamburger');
-        // const navLinks = document.querySelector('.nav-links');
-        // const links = document.querySelectorAll('.nav-links li');
-
-        // hamburger.addEventListener("click", () => {
-        //     navLinks.classList.toggle("open");
-        //     links.forEach(link =>{
-        //         link.classList.toggle("fade");
-        //     });
-        // });
-    
-                       
-        return (
-            // <body>
-    <nav>
-      <div class="hamburger" onClick={this.clickBurgerHandler}>
-        <div class="line"></div>
-        <div class="line"></div>
-        <div class="line"></div>
-      </div>
-      <ul class="nav-links">
-        <li><a href="#">About</a></li>
-        <li><a href="#">Larrakia People</a></li>
-        <li><a href="#">Gulumirrgin Dictionary for Kids</a></li>
-      </ul>
-    </nav>
-
-    /* <section class="landing">
-    <h1>Hello World</h1>
-    </section> */
-
- 
-        )
-
-
-    }
-
+  render() {
+    return (
+      <nav>
+        <div className="hamburger" onClick={this.clickBurgerHandler}>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+          <div className="hamburger-line"></div>
+        </div>
+        <ul className={"nav-links " + (this.state.isShowing && "open")}>
+          <li><a href="#">Gulumirrgin Dictionary for Kids</a></li>
+          <li><a href="#">Larrakia People</a></li>
+          <li><a href="#">About </a></li>
+        </ul>
+      </nav>
+    )
+  }
 }
-
 
 export default BurgerMenu
