539204964b9bbd484b1224ff2ec95861c7cab91d
Richard Kwon <rkminjae@gmail.com>
2020-05-12 10:25:58 +1200

    Add About component

---
+++ client/components/About.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class About extends Component {
+    render() {
+        return (
+            <div className="container">
+                <h1>About</h1>
+                <h2>Motivation</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vel reiciendis debitis illo quis cumque consectetur suscipit mollitia, numquam accusantium ea enim velit molestiae similique non accusamus voluptatum at excepturi fugiat itaque doloremque ipsam facilis eligendi. Voluptas qui, corrupti quam eaque tempora provident aliquid laudantium fuga possimus illum! Officiis aut, accusamus alias quaerat provident, officia odit tenetur possimus autem dolor iste. Quaerat, eos error nisi quo voluptatibus commodi cum. Architecto alias vel quam eum adipisci officia pariatur facilis delectus quae reprehenderit assumenda, quas harum dicta autem sed soluta distinctio aspernatur recusandae? Reiciendis optio, soluta consequuntur sequi rerum voluptatum omnis itaque enim?</p>
+
+                <h2>Goals</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente iure quam, itaque et pariatur necessitatibus repudiandae eveniet reiciendis neque nemo assumenda, sed, doloribus praesentium voluptatum distinctio ullam magnam facilis blanditiis esse tenetur eum similique modi. Molestias, accusamus recusandae sequi quia ex sit ab dolor fugiat accusantium fugit minima ipsa repudiandae.</p>
+
+                <h2>Meet the Team</h2>
+                <ul>
+                    <li>Laniyuk - Director</li>
+                    <li>Simone - Manager</li>
+                    <li>Zac - Git/Deployment Lead</li>
+                    <li>Boyd - Backend Lead</li>
+                    <li>Richard - Frontend Lead</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default About
