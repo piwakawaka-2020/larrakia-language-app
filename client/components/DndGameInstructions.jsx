84cfeeaa0ee7e53e324ff641c8eebca7b1db4bbd
Richard Kwon <rkminjae@gmail.com>
2020-05-11 12:31:11 +1200

    Add DndGameInstructions component

---
+++ client/components/DndGameInstructions.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const DndGameInstructions = () => {
+    return (
+        <div>
+            <h3>How to Play:</h3>
+            <ol>
+                <li>Drag the image into the box and try to match it with the Gulumirrgin word</li>
+                <li>If you get it right you go onto the next word</li>
+                <li>If you get it wrong you get to try again until you get it right</li>
+                <li>Get 10 right and you win the game! Congratulations!</li>
+            </ol>
+        </div>
+    )
+}
+
+export default DndGameInstructions
