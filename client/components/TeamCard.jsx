4d6a4a5bdab586e8c43929f3c5ae716cc3bd91e8
Richard Kwon <rkminjae@gmail.com>
2020-05-13 11:49:49 +1200

    Add props to TeamCard components

--- client/components/TeamCard.jsx
+++ client/components/TeamCard.jsx
@@ -5,15 +5,15 @@ class TeamCard extends Component {
     render() {
         return (
             <div className="p-3"> 
-            <div className="card mb-3 team-card">
+            <div className="card border-dark mb-3 team-card">
                 <div className="row no-gutters">
                     <div className="col-md-4">
-                        <img src="https://www.mountaineers.org/images/placeholder-images/placeholder-contact-profile/image" className="card-img" alt="" onMouseLeave={this.cardHoverHandler}/>
+                        <img src={this.props.profilePic === undefined ? "https://www.mountaineers.org/images/placeholder-images/placeholder-contact-profile/image" : this.props.profilePic} className="card-img" alt=""/>
                     </div>
                     <div className="col-md-8">
                         <div className="card-body">
-                            <h5 className="card-title">Name</h5>
-                            <p className="card-text">Role description</p>
+                            <h5 className="card-title">{this.props.name}</h5>
+                            <h5 className="card-text">{this.props.role}</h5>
                         </div>
                     </div>
                 </div>
