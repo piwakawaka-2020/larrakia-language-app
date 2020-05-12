44d1e9ca4e7e2f69c4b66bd7927bf404e8016a36
Zachary <zachildred@gmail.com>
2020-05-13 10:28:16 +1200

    Add ability to sort dictionary by alphabetical order in either language

--- client/components/Dictionary.jsx
+++ client/components/Dictionary.jsx
@@ -2,48 +2,35 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import DictionaryEntry from './DictionaryEntry'
+import { compareEnglishWords, compareGulumirrginWords } from '../utilities/sorting'
 
 class Dictionary extends React.Component {
   state = {
-    //wordList: [],
-    isSortedForGulumirrgin: true
+    sortForGulumirrgin: true
   }
-
-  // componentDidUpdate () {
-  //   this.setState({
-  //     wordList: [...this.props.words]
-  //   })
-  // }
 
   handleClick = event => {
-    console.log("HANDLING CLICK")
-    if (isSortedForGulumirrgin) {
-      console.log("BEEP")
-      this.props.wordList.sort(compareEnglishWords)
-      isSortedForGulumirrgin = false
-    } else {
-      console.log("BOOP")
-      this.props.wordList.sort(compareGulumirrginWords)
-      isSortedForGulumirrgin = true
-    }
+    this.setState({ sortForGulumirrgin: !this.state.sortForGulumirrgin })
   }
 
-
   render () {
+    let sortedWords = [...this.props.words]
     let dictionaryEntries = []
   
-    if (this.props.wordList.length > 0) {
-      this.props.wordList.sort(compareGulumirrginWords)
-  
-      dictionaryEntries = this.props.wordList.map(word => {
-        return <DictionaryEntry key={word.id} word={word} />
-      })
+    if (this.state.sortForGulumirrgin) {
+      sortedWords.sort(compareGulumirrginWords)
+    } else {
+      sortedWords.sort(compareEnglishWords)
     }
+    
+    dictionaryEntries = sortedWords.map(word => {
+      return <DictionaryEntry key={word.id} word={word} />
+    })
 
     return (
       <div className="dictionary">
         <h2>DICTIONARY</h2>
-        <button onClick={this.handleClick}>Sort by {this.props.isSortedForGulumirrgin ? "English" : "Gulumirrgin"} alphabetical order?</button>
+        <button onClick={this.handleClick}>Sort by {this.state.sortForGulumirrgin ? "English" : "Gulumirrgin"} alphabetical order?</button>
         <ul>
           {dictionaryEntries}
         </ul>
@@ -54,29 +41,8 @@ class Dictionary extends React.Component {
 
 const mapStateToProps = state => {
   return {
-    wordList: [...state.words]
+    words: state.words
   }
-}
-
-//Generic function for sorting an array by alphabetical order
-const compareWords = (a, b) => {
-  if (a < b) {
-    return -1
-  } else if (a > b) {
-    return 1
-  } else {
-    return 0
-  }
-}
-
-//Will sort an array of word objects by alphabetical order of their Gulumirrgin Word
-const compareGulumirrginWords = (a, b) => {
-  return compareWords(a.gulumirrginWord.toUpperCase(), b.gulumirrginWord.toUpperCase())
-}
-
-//Will sort an array of word objects by alphabetical order of their English Word
-const compareEnglishWords = (a, b) => {
-  return compareWords(a.englishWord.toUpperCase(), b.englishWord.toUpperCase())
 }
 
 export default connect(mapStateToProps)(Dictionary)
