34fca618ee2414be8c4b4e64e3f757da8c84687e
Boyd_Thurston <boydthurston@gmail.com>
2020-05-13 15:53:38 +1200

    centered component

--- client/components/Loading.jsx
+++ client/components/Loading.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Loading = props => {
   return (
-    <div className="container h-100">
-      <div mx-auto my-auto>
+    <div className="container h-100 ">
+      <div className="d-flex justify-content-center align-items-center">
         <img id="loading-gif" src="/gifs/animated-circle.gif" />
       </div>
     </div>
