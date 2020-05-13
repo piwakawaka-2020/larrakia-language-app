abefe0ce968dae5b91cadafbb8d887824fdbf235
Richard Kwon <rkminjae@gmail.com>
2020-05-13 16:48:58 +1200

    Add left padding to instructions class in Instructions component

--- client/components/Instructions.jsx
+++ client/components/Instructions.jsx
@@ -4,7 +4,7 @@ const Instructions = ({ children }) => {
     return (
         <div className="instructions">
             <h3>How to Play:</h3>
-            <ol>
+            <ol className="">
                 {/* Wrap <li> tags with <Instructions> tags */}
                 {children}
             </ol>
